3ff77889a0c250a85d1fdb6f8b68b179382d1d6f
jairocket <jailsonanjos@hotmail.com>
2021-09-13 17:28:00 -0300

    chamadas http

--- src/components/RepositoryItem.jsx
+++ src/components/RepositoryItem.jsx
@@ -1,10 +1,11 @@
 
 export function RepositoryItem(props){
+    console.log(props)
     return(
         <li>
-            <strong>{props.repository ?? 'Default'}</strong>
-            <p>Forms in React</p>
-            <a href=''>
+            <strong>{props.repository.name}</strong>
+            <p>{props.repository.description}</p>
+            <a href={props.repository.html_url}>
                 Acessar Repositório
             </a>
         </li>
